3e3b0fdaacd0e04e3d696d6fd3a5c894eb2bdb86
hirokimituya <81066421+hirokimituya@users.noreply.github.com>
2021-05-01 11:39:59 +0900

    ペジネーションのバグ修正

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -68,7 +68,7 @@ export default {
     data() {
         return {
             mdiChevronRight, mdiChevronLeft,
-            page_data: this.prop,
+            page_data: this.page,
             success: this.snackbar,
         }
     },
